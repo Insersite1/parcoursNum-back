00c3ab25782fcd4e7204be3db86851b5d5a555da
SokhnaDieye <dieyesokhna2021@gmail.com>
2025-01-16 00:09:37 +0000

    modifiet

--- app/Http/Controllers/JeuneController.php
+++ app/Http/Controllers/JeuneController.php
@@ -407,9 +407,22 @@ public function updatePassword(Request $request)
 
 
 
-    /*
-    Description modification du jeune complet
-    */
+    /**
+     * Description: Mettre à jour les informations complètes d'un utilisateur avec le rôle "Jeune".
+     * Méthode: PUT ou PATCH
+     * Entrée:
+     *   - id (identifiant de l'utilisateur),
+     *   - Données utilisateur (nom, prénom, email, téléphone, etc.).
+     * Sortie:
+     *   - Message de succès + status 200 en cas de mise à jour réussie,
+     *   - Message d'erreur de validation + status 422 si les données sont invalides,
+     *   - Message d'erreur + status 404 si l'utilisateur n'existe pas,
+     *   - Message d'erreur + status 500 en cas d'erreur serveur.
+     * Particularité:
+     *   - Prise en charge de l'upload et la suppression de l'avatar.
+     *   - Conversion automatique des champs booléens.
+     */
+
     public function updateJeuneComplet(Request $request, $id)
     {
         try {
